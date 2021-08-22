c75bce805785069a6b5a48cce6db89fe42924526
Phil Webster <pwebster33@gmail.com>
2021-08-22 07:14:05 -0400

    Remove extra files

--- classes/admin/Admin_Menu.php
+++ classes/admin/Admin_Menu.php
@@ -3,74 +3,78 @@
 namespace Yardline\Admin;
 
 
-use Yardline\Admin\Settings_Page;
+//use Yardline\Admin\Settings_Page;
 use Yardline\Admin\Score_Board_Page;
 use function Yardline\isset_not_empty;
 
 /**
- *
  * Admin Manage
+ *
  * @package Yardline\Admin
  */
-class Admin_Menu {
+class Admin_Menu
+{
 
-	/**
-	 * @var Admin_Page[]
-	 */
-	protected $pages = [];
+    /**
+     * @var Admin_Page[]
+     */
+    protected $pages = [];
 
 
-	/**
-	 * Manager constructor.
-	 */
-	public function __construct() {
-		add_action( 'init', [ $this, 'init_admin' ], 25 );
-	}
+    /**
+     * Manager constructor.
+     */
+    public function __construct()
+    {
+        add_action('init', [ $this, 'init_admin' ], 25);
+    }
 
-	/**
-	 * Setup the base DBs for the plugin
-	 */
-	public function init_admin() {
-		$this->score_board  = new Score_Board_Page();
-		$this->goals = new Goals_Page();
-	    $this->settings  = new Settings_Page();
-	
-	}
+    /**
+     * Setup the base DBs for the plugin
+     */
+    public function init_admin()
+    {
+        $this->score_board  = new Score_Board_Page();
+        
+    }
 
-	/**
-	 * Set the data to the given value
-	 *
-	 * @param $key string
-	 *
-	 * @return Admin_Page
-	 */
-	public function get_page( $key ) {
-		return $this->$key;
-	}
+    /**
+     * Set the data to the given value
+     *
+     * @param $key string
+     *
+     * @return Admin_Page
+     */
+    public function get_page( $key )
+    {
+        return $this->$key;
+    }
 
-	/**
-	 * Magic get method
-	 *
-	 * @param $key string
-	 *
-	 * @return bool|Admin_Page
-	 */
-	public function __get( $key ) {
-		if ( isset_not_empty( $this->pages, $key ) ) {
-			return $this->pages[ $key ];
-		}
+    /**
+     * Magic get method
+     *
+     * @param $key string
+     *
+     * @return bool|Admin_Page
+     */
+    public function __get( $key )
+    {
+        if (isset_not_empty($this->pages, $key) ) {
+            return $this->pages[ $key ];
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Set the data to the given value
-	 *
-	 * @param $key string
-	 * @param $value Admin_Page
-	 */
-	public function __set( $key, $value ) {
-		$this->pages[ $key ] = $value;
-	}
+    /**
+     * Set the data to the given value
+     *
+     * @param $key   string
+     * @param $value Admin_Page
+     */
+    public function __set( $key, $value )
+    {
+        $this->pages[ $key ] = $value;
+    }
 
 }
--- src/js/components/CurrentRange.js
+++ src/js/components/CurrentRange.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { formatDate } from './helpers.js';
-class CurrentRange extends React.Component {
-    render() {
-        return (
-            <button className="yardline-current-range" onClick={this.props.toggleClass} >
-                <span className="yl-start-date">{this.props.range.startDate.toDateString()}</span>
-                <span> to </span>
-                <span className="yl-end-date">{this.props.range.endDate.toDateString()}</span>     
-            </button>
-        );
-    }
-
+import React from "react";
+import { formatDate } from "./helpers.js";
+function CurrentRange(props) {
+  return (
+    <button className="yardline-current-range" onClick={props.toggleClass}>
+      <span className="yl-start-date">
+        {props.range.startDate.toDateString()}
+      </span>
+      <span> to </span>
+      <span className="yl-end-date">{props.range.endDate.toDateString()}</span>
+    </button>
+  );
 }
 
 export default CurrentRange;
